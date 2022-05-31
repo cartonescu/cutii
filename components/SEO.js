1db91432a55ec9f2eded65ffd5f1b51b5ce76689
Cartonescu <105561307+cartonescu@users.noreply.github.com>
2022-05-31 19:48:20 +0100

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function SEO({ title, description }) {
   return (
     <Head>
-      <title>{title}</title>
+      <title>Cutii de carton - Cutii carton ambalare - Cutii de carton cu autoformare</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
     </Head>

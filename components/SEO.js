1c8052ce37eb366867802740a9bab0a09165fa62
Cartonescu <105561307+cartonescu@users.noreply.github.com>
2022-05-31 19:50:57 +0100

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -4,7 +4,7 @@ export default function SEO({ title, description }) {
   return (
     <Head>
       <title>Cutii de carton - Cutii carton ambalare - Cutii de carton cu autoformare</title>
-      <meta name="description" content={description} />
+      <meta name="description" content=Cutii de carton pentru ambalarea produselor tale. Cele mai bune cutii de carton cu autoformare pentru ambalare si expediere. Cutii de carton cartonescu.ro />
       <meta property="og:title" content={title} />
     </Head>
   );
